5dae65d965472acf6a777906a299b4f725ae5ba5
alattimer <alattimer@users.noreply.github.com>
2023-03-09 14:00:07 -0700

    chadgpt

--- GUI/Main.py
+++ GUI/Main.py
@@ -2,6 +2,6 @@ from View import View, CTk
 
 if __name__ == '__main__':
     root = CTk.CTk()  # root object (pop up window)
-    root.title("Ground Control Station")
+    root.title("ChadGPT")
     view = View(root, master=root)
     root.mainloop()
