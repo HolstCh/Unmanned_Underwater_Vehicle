a7e0b1eae76e4d9b9f60f95d3fae3513ebf8d5f5
Chad Holst <81201946+HolstCh@users.noreply.github.com>
2023-03-11 02:44:53 -0700

    Add files via upload

---
+++ Companion/Servo.py
@@ -0,0 +1,8 @@
+# servo class which assigns channel and pwm values for each servo
+class Servo:
+    # recommended pwm values are the following: 1100: full reverse, 1500: stopped/default, 1900: full forward supplied to ESC
+    def __init__(self, master, servo_n, pwm_range=[1100,1500,1900]):
+        self.master = master
+        self.servo_n = servo_n
+        self.pwm_range = pwm_range
+        self.min, self.default, self.max = pwm_range
