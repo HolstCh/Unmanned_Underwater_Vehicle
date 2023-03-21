5c59259d262425b2ce91bb9d63040cdf5bdd2ce4
HolstCh <chad.holst@Hotmail.com>
2023-03-20 23:55:52 -0600

    adjusted sliders/grid to make it easier to add UI components, moved switches

--- GUI/Main.py
+++ GUI/Main.py
@@ -2,6 +2,6 @@ from View import View, CTk
 
 if __name__ == '__main__':
     root = CTk.CTk()  # root object (pop up window)
-    root.title("ChadGPT")
+    root.title("Ground Control Station")
     view = View(root, master=root)
     root.mainloop()
