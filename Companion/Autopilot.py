2e0df8f2396047a4ec115904129b1d22e07fd009
Chad Holst <81201946+HolstCh@users.noreply.github.com>
2023-03-23 21:19:08 -0600

    cleaned up/removed main()

--- Companion/Autopilot.py
+++ Companion/Autopilot.py
@@ -2,16 +2,8 @@ import sys
 from time import sleep
 from pymavlink import mavutil
 from Mavlink import Mavlink
+import os
 
-# thruster class which assigns channel and pwm values for it's corresonding servo
-class Thruster:
-    # recommended pwm values are the following: 1000: full reverse, 1500: stopped/default, 2000: full forward supplied to ESC
-    def __init__(self, master, servo_n, pwm_range=[1000,1500,2000]):
-        self.master = master
-        self.servo_n = servo_n
-        self.pwm_range = pwm_range
-        self.min, self.default, self.max = pwm_range
-        
 # Communication between companion and autopilot
 class Autopilot:
     def __init__(self, master):
@@ -31,11 +23,13 @@ class Autopilot:
                 print(error)
 
     def read_param(self, param):
-        """self.master.mav.param_request_read_send(self.master.target_system, self.master.target_component, str.encode(param), -1)
-        message = self.master.recv_match(type='PARAM_VALUE', blocking=True).to_dict()
-        print('name: %s\tvalue: %d' % (message['param_id'], message['param_value']))"""
+        self.master.mav.param_request_read_send(self.master.target_system, self.master.target_component, str.encode(param), -1)
         msg = self.master.recv_match(type=param, blocking=True).to_dict()
         print(msg)
+    
+    def get_param_dict(self, param):
+        self.master.mav.param_request_read_send(self.master.target_system, self.master.target_component, str.encode(param), -1)
+        return self.master.recv_match(type=param, blocking=True).to_dict()
 
     def arm(self):
             self.master.mav.command_long_send(
@@ -60,8 +54,8 @@ class Autopilot:
             print('Motors disarmed!\n')
 
     def set_config(self):
-        # set # of FMU PWM outputs to motor controller which are pins: AUX1, AUX2, AUX3, and AUX4
-        self.master.BRD_PWM_COUNT = 6
+        # set version to Mavlink 2
+        os.environ['MAVLINK20'] = '1'
 
     def set_mode(self, mode):
         # Check if mode is available
@@ -101,9 +95,11 @@ class Autopilot:
                                                    self.master.target_component,
                                                    mavutil.mavlink.MAV_CMD_DO_SET_SERVO, 0,
                                                    channel, pwm,
-                                                   0, 0, 0, 0, 0)
+                                                  0, 0, 0, 0, 0)
+
+    # RC channels are for further development involving joystick/controller device using RC reciever                                              
     def set_default_rc_channels(self):
-        self.rc_channel_values = [65535 for i in range(18)] # set first 9 RC channels, 65535 means ignore; otherwise use pwm values
+        self.rc_channel_values = [65535 for i in range(18)] # set first 18 RC channels, 65535 means ignore; otherwise use pwm values
 
     def set_rc_channel_pwm(self, channel_id, pwm=1500):
 
@@ -134,79 +130,3 @@ class Autopilot:
     
     def set_lateral(self, pwm):
         self.set_rc_channel_pwm(6, pwm)
-
-if __name__ == '__main__':
-    
-    # establish mavlink connection to autopilot through serial
-    mav_autopilot = Mavlink("/dev/ttyACM0", mavutil.mavlink.MAV_TYPE_SUBMARINE, mavutil.mavlink.MAV_AUTOPILOT_PX4, 
-    0, 0, 0, 1, mavutil.mavlink.MAV_COMP_ID_AUTOPILOT1)
-
-    # instantiate autopilot object and pass autopilot-mavlink connection
-    autopilot = Autopilot(mav_autopilot.get_connection())
-   
-    # send HEARTBEAT message repeatedly from Companion to Autopilot every 1s or else Autopilot goes into failsafe mode
-    mav_autopilot.start_heartbeat()
-
-    # Wait for a heartbeat from the PX4 before sending commands
-    autopilot.master.wait_heartbeat()
-
-    # print mavlink system that made connection: Companion system ID = 127, GCS system ID = 255, Autopilot system ID = 1
-    print("Source heartbeat from companion system: (system %u component %u)" % (autopilot.master.source_system, autopilot.master.source_component))
-    print("Target heartbeat from autopilot system: (system %u component %u)" % (autopilot.master.target_system, autopilot.master.target_component))
-
-    # set configurations for autopilot
-    autopilot.set_config()
-
-    # set mode: ['STABILIZE', 'ACRO', 'ALT_HOLD', 'AUTO', 'GUIDED', 'CIRCLE', 'SURFACE', 'POSHOLD', 'MANUAL']
-    # note: manual mode used for RC inputs which use servo functions to map to servo output channels.
-    autopilot.set_mode('MANUAL')
-    
-    RC_MODE = 0
-    SERVO_MODE = 1
-
-    if RC_MODE:
-        # set RC channels all to 65355 which means ignore
-        autopilot.set_default_rc_channels()
-
-        # autopilot.arm()
-        # set each input RC channel PWM 
-        autopilot.set_pitch(1160) # RC input channel 1
-        autopilot.set_roll(1160) # RC input channel 2
-        autopilot.set_throttle(1160) # RC input channel 3
-        autopilot.set_yaw(1160) # RC input channel 4
-        autopilot.set_forward(1180) # RC input channel 5
-        autopilot.set_lateral(1180) # RC input channel 6
-        autopilot.master.SERVO9_FUNCTION = 1 # enable RCPassThru on AUX servo output channel 9
-        autopilot.master.SERVO10_FUNCTION = 1 # enable RCPassThru on AUX servo output channel 10
-        autopilot.master.SERVO11_FUNCTION = 1 # enable RCPassThru on AUX servo which is output channel 11
-        autopilot.read_param('RC_CHANNELS') # only works with MAVlink V1.0?
-        autopilot.read_param('SERVO_OUTPUT_RAW')
-        # autopilot.disarm()
-
-    if SERVO_MODE:
-        autopilot.arm()
-        autopilot.master.SERVO9_FUNCTION = 0 # disable servo functions to use set_servo()
-        autopilot.master.mav.command_long_send(autopilot.master.target_system, autopilot.master.target_component, mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE, 0, 0, 300, 0, 0, 0, 0, 0, 1)
-        print(mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE)
-        ack = False
-        while not ack:
-            ack_msg = autopilot.master.recv_match(type='COMMAND_ACK', blocking=True)
-            ack_msg = ack_msg.to_dict()
-            print(mavutil.mavlink.enums['MAV_RESULT'][ack_msg['result']].description)
-            print(ack_msg)
-            break
-            
-        # create a thruster instance on servo_9 (AUX output 1 which is same as servo output channel 9)
-        thruster1 = Thruster(autopilot.master, 9)
-        # create a thruster instance on servo_1 (AUX output 2 which is same as servo output channel 10)
-        # thruster2 = Thruster(autopilot.master, 10)
-        # create a thruster instance on servo_1 (AUX output 3 which is same as servo output channel 11)
-        # thruster3 = Thruster(autopilot.master, 11)
-
-        for i in range(3):
-            autopilot.master.set_servo(thruster1.servo_n, thruster1.default)
-            autopilot.read_param('SERVO_OUTPUT_RAW')
-            sleep(1)
-
-        autopilot.stop_servo(9)
-        autopilot.disarm()
