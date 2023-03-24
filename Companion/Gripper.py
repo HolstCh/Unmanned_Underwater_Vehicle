e7d903cbfe778054d5ab2b18ae973b528b3b5a8a
Chad Holst <81201946+HolstCh@users.noreply.github.com>
2023-03-24 01:00:46 -0600

    removed autopilot connection

--- Companion/Gripper.py
+++ Companion/Gripper.py
@@ -1,8 +1,9 @@
+# Gripper class which assigns channel and pwm values for each gripper
+
 class Gripper:
-    # recommended pwm values are the following: closed state: 1100-1470, default (previous state): 1500, open state: 1530-1900
-    def __init__(self, master, servo_n, pwm_range=[1200, 1500, 1800]):
-        self.master = master
-        self.servo_n = servo_n
+    # recommended pwm values are the following: closed state: 1100-1470, default (previous state): 1500 open state: 1530-1900
+    def __init__(self, gripper_n, pwm_range=[1200, 1500, 1800]):
+        self.gripper_n = gripper_n
         self.pwm_range = pwm_range
         self.min, self.default, self.max = pwm_range
 
