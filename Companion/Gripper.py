23a82edeb5ec6403d03922bdaf73ca57f8850e97
Chad Holst <81201946+HolstCh@users.noreply.github.com>
2023-03-23 21:26:25 -0600

    update pwm values

--- Companion/Gripper.py
+++ Companion/Gripper.py
@@ -1,5 +1,5 @@
 class Gripper:
-    # recommended pwm values are the following: closed state: 1100-1470, default (previous state): 1500 open state: 1530-1900
+    # recommended pwm values are the following: closed state: 1100-1470, default (previous state): 1500, open state: 1530-1900
     def __init__(self, master, servo_n, pwm_range=[1200, 1500, 1800]):
         self.master = master
         self.servo_n = servo_n
