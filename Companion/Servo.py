30fa54d55c7246e61e5cd78315acfe6762b43f9a
Chad Holst <81201946+HolstCh@users.noreply.github.com>
2023-03-24 01:05:10 -0600

    update pwm values

--- Companion/Servo.py
+++ Companion/Servo.py
@@ -1,8 +1,8 @@
-# servo class which assigns channel and pwm values for each servo
+# Servo class which assigns channel and pwm values for each servo
+
 class Servo:
-    # recommended pwm values are the following: 1100: full reverse, 1500: stopped/default, 1900: full forward
-    def __init__(self, master, servo_n, pwm_range=[1100,1500,1900]):
-        self.master = master
+    # recommended pwm values are the following: 900: full rotation counterclockwise, 1500: neutral position/default, 2100: full rotation clockwise
+    def __init__(self, servo_n, pwm_range=[900,1500,2100]):
         self.servo_n = servo_n
         self.pwm_range = pwm_range
         self.min, self.default, self.max = pwm_range
