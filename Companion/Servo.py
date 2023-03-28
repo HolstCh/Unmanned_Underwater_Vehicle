912a438c9e5a4d1c91c44c6d10f12833070628b9
Chad Holst <81201946+HolstCh@users.noreply.github.com>
2023-03-27 21:29:28 -0600

    moved pwm range so developer can set conveniently

--- Companion/Servo.py
+++ Companion/Servo.py
@@ -2,7 +2,7 @@
 
 class Servo:
     # recommended pwm values are the following: 900: full rotation counterclockwise, 1500: neutral position/default, 2100: full rotation clockwise
-    def __init__(self, servo_n, pwm_range=[900,1500,2100]):
+    def __init__(self, servo_n, pwm_range):
         self.servo_n = servo_n
         self.pwm_range = pwm_range
         self.min, self.default, self.max = pwm_range
