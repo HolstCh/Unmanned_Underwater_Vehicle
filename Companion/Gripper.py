ae4534f7b28927917d559d7190ed34ed903d3c0c
Chad Holst <81201946+HolstCh@users.noreply.github.com>
2023-03-27 21:28:08 -0600

    moved pwm range so developer can set conveniently

--- Companion/Gripper.py
+++ Companion/Gripper.py
@@ -2,7 +2,7 @@
 
 class Gripper:
     # recommended pwm values are the following: closed state: 1100-1470, default (previous state): 1500 open state: 1530-1900
-    def __init__(self, gripper_n, pwm_range=[1200, 1500, 1800]):
+    def __init__(self, gripper_n, pwm_range):
         self.gripper_n = gripper_n
         self.pwm_range = pwm_range
         self.min, self.default, self.max = pwm_range
