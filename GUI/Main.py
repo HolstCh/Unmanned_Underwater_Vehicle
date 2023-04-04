949ecab67ba01bf30c1647bfb2a9b223d49446a9
Chad Holst <81201946+HolstCh@users.noreply.github.com>
2023-04-03 23:04:20 -0600

    Working IMU data, 3 servo and 2 gripper channels

--- GUI/Main.py
+++ GUI/Main.py
@@ -2,8 +2,10 @@ from View import View, CTk
 
 if __name__ == '__main__':
     root = CTk.CTk()  # root object (pop up window)
-    root.geometry("850x700")
+    #root.geometry("1200x900")
     root.resizable(0,0)
     root.title("Ground Control Station")
     view = View(root, master=root)
+    view.update_label()
     root.mainloop()
+    
