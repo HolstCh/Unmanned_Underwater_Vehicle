01d49e1340d6c4289e141c336d4a368598d38af1
Chad Holst <81201946+HolstCh@users.noreply.github.com>
2023-04-13 16:15:53 -0600

    Documentation

--- GUI/Main.py
+++ GUI/Main.py
@@ -1,11 +1,10 @@
 from View import View, CTk
 
+# point of execution for the GUI package. The file creates a View and CustomTkinter object to run the GUI
 if __name__ == '__main__':
     root = CTk.CTk()  # root object (pop up window)
-    #root.geometry("1200x900")
     root.resizable(0,0)
     root.title("Ground Control Station")
     view = View(root, master=root)
     view.update_label()
     root.mainloop()
-    
