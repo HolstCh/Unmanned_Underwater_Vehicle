e255de57ef2bd4092b8ada0b8d3a1edc5284748e
Chad Holst <81201946+HolstCh@users.noreply.github.com>
2023-04-13 16:02:05 -0600

    Documentation

--- Companion/Autopilot.py
+++ Companion/Autopilot.py
@@ -4,11 +4,13 @@ from pymavlink import mavutil
 from Mavlink import Mavlink
 import os
 
-# Communication between companion and autopilot
+""" Autopilot class is responsible for taking input from Model.py, sending commands directly to the PX4, and recieving data from the PX4. 
+Many functions were altered or taken from this website: https://www.ardusub.com/developers/pymavlink.html """
 class Autopilot:
     def __init__(self, master):
         self.master = master
 
+    # prints all parameters on the PX4 to the command line
     def read_params(self):
         self.master.mav.param_request_list_send(
         self.master.target_system, self.master.target_component
@@ -22,15 +24,18 @@ class Autopilot:
             except Exception as error:
                 print(error)
 
+    # prints a specific parameter on the PX4 to the command line
     def read_param(self, param):
         self.master.mav.param_request_read_send(self.master.target_system, self.master.target_component, str.encode(param), -1)
         msg = self.master.recv_match(type=param, blocking=True).to_dict()
         print(msg)
     
+    # returns a dicionary data structure corresponding to a parameter
     def get_param_dict(self, param):
         self.master.mav.param_request_read_send(self.master.target_system, self.master.target_component, str.encode(param), -1)
         return self.master.recv_match(type=param, blocking=True).to_dict()
 
+    # arms motors, used as a safety feature
     def arm(self):
             self.master.mav.command_long_send(
             self.master.target_system,
@@ -42,6 +47,7 @@ class Autopilot:
             self.master.motors_armed_wait()
             print('Motors armed!\n')
 
+    # disarms motors, used as a safety feature
     def disarm(self):
             self.master.mav.command_long_send(
             self.master.target_system,
@@ -53,6 +59,7 @@ class Autopilot:
             self.master.motors_disarmed_wait()
             print('Motors disarmed!\n')
 
+    # set_config() allows all servo output channels to be able to use the MAV_CMD_DO_SET_SERVO command
     def set_config(self):
         # set version to Mavlink 2
         os.environ['MAVLINK20'] = '1'
@@ -62,7 +69,10 @@ class Autopilot:
         self.master.SERVO12_FUNCTION = 0
         self.master.SERVO13_FUNCTION = 0
         self.master.SERVO14_FUNCTION = 0
+        self.master.SERVO15_FUNCTION = 0
+        self.master.SERVO16_FUNCTION = 0
 
+    # set flight mode (used with RC/joystick)
     def set_mode(self, mode):
         # Check if mode is available
         if mode not in self.master.mode_mapping():
@@ -90,12 +100,26 @@ class Autopilot:
             print(mavutil.mavlink.enums['MAV_RESULT'][ack_msg['result']].description)
             break
 
+    """
+        set_servo() takes channel and pwm computed by Model.py and sends the pwm values to the PX4 and corresponding channel
+        for the electronic component
+
+        Inputs:
+        channel: corresponds to AUX pin/channel on the power management board
+        pwm: corresponds to desired pwm value for servo
+
+        Outputs:
+        MAV_CMD_DO_SET_SERVO command directly to PX4 using MAVlink
+
+    """       
     def set_servo(self, channel, pwm):
+        print(channel, pwm)
         self.master.mav.command_long_send(self.master.target_system,
                                                    self.master.target_component,
                                                    mavutil.mavlink.MAV_CMD_DO_SET_SERVO, 0,
                                                    channel, pwm,
                                                    0, 0, 0, 0, 0)
+        
     def stop_servo(self, channel, pwm=1500):
         self.master.mav.command_long_send(self.master.target_system,
                                                    self.master.target_component,
@@ -103,7 +127,7 @@ class Autopilot:
                                                    channel, pwm,
                                                   0, 0, 0, 0, 0)
 
-    # RC channels are for further development involving joystick/controller device using RC reciever                                              
+    # RC channels are for further development involving joystick/controller device                                            
     def set_default_rc_channels(self):
         self.rc_channel_values = [65535 for i in range(18)] # set first 18 RC channels, 65535 means ignore; otherwise use pwm values
 
@@ -118,7 +142,8 @@ class Autopilot:
             self.master.target_system,                # target_system
             self.master.target_component,             # target_component
             *self.rc_channel_values)                  # RC channel list, in microseconds.
-            
+
+    # set_DOF functions may be useful for taking RC Input and setting the pwm values to each channel in the future      
     def set_pitch(self, pwm):
         self.set_rc_channel_pwm(1, pwm)
     
